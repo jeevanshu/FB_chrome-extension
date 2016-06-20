6c20107ab175a4b6ccb9bf5ddabe8ac8b4988693
jeevanshu <jeevanshumalhotra@gmail.com>
2016-06-20 18:38:25 +0530

    Add files via upload

---
+++ mobile_feed/popup.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+//document.addEventListener('DOMContentLoaded', function() {
+ /* var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('mouseover', function() {*/
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      var f = d.createElement('form');
+      f.action = 'https://mbasic.facebook.com/home.php?_rdr';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab.url;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  //}, false);
+}, false;
